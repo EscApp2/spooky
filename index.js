4e345f712c9f94bee1d891edeca75037e7552cab
EscApp2 <max.straustrup@gmail.com>
2016-11-13 21:42:39 +0300

    1

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var util         = require('util');
+
 var app = express();
 app.use(express.logger());
 
@@ -6,9 +8,38 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+  
+  
+});
+
+app.get('/apps/', function(request, response) {
+  response.send(echo(request));
+  
+  
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+//http://stackoverflow.com/questions/4816099/chrome-sendrequest-error-typeerror-converting-circular-structure-to-json
+function echo(data){
+	if(typeof data == "object"){
+		var str = util.inspect(data, {depth: null});
+		str = str
+			.replace(/<Buffer[ \w\.]+>/ig, '"buffer"')
+			.replace(/\[Function]/ig, 'function(){}')
+			.replace(/\[Circular]/ig, '"Circular"')
+			.replace(/\{ \[Function: ([\w]+)]/ig, '{ $1: function $1 () {},')
+			.replace(/\[Function: ([\w]+)]/ig, 'function $1(){}')
+			.replace(/(\w+): ([\w :]+GMT\+[\w \(\)]+),/ig, '$1: new Date("$2"),')
+			.replace(/(\S+): ,/ig, '$1: null,')
+			.replace(/\n/ig, "<br/>");
+
+		return  "<pre>"+JSON.stringify(str, null, 4)+"</pre>";
+	}else{
+		return  "<pre>"+data+"</pre>";
+	}
+} 
